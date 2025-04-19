3694a08055192e1ee93a9cdfe2671d6a01c4b441
ZacharyWickliffe <zwickliffe@horizon.csueastbay.edu>
2025-04-19 12:33:03 -0700

    Added ATM & Teller classes w/ empty methods
    Added ATM & Teller classes w/ empty methods

---
+++ implentation/ATM.java
@@ -0,0 +1,44 @@
+public class ATM {
+	public ATM() {
+
+	}
+	public void parseRecMessage() {
+
+	}
+	public void login() {
+
+	}
+	public void logout() {
+
+	}
+	public void autoLogout() {
+
+	}
+	public void selectAccount() {
+
+	}
+	public void exitAccount() {
+
+	}
+	public void sendMessage(Message message) {
+
+	}
+	public void withdraw() {
+
+	}
+	public void viewBalance() {
+
+	}
+	public void viewTransactionHistory() {
+
+	}
+	public void deposit() {
+
+	}
+	public void getCurrReserve() {
+
+	}
+	public void updateCurrReserve() {
+
+	}
+}
---
+++ implentation/Teller.java
@@ -0,0 +1,64 @@
+
+public class Teller {
+	public Teller() {
+		
+	}
+	
+	public void parseRecMessage() {
+		
+	}
+	public void login() {
+		
+	}
+	public void logout() {
+		
+	}
+	public void selectCustomer() {
+		
+	}
+	public void exitCustomer() {
+		
+	}
+	public void selectAccount() {
+		
+	}
+	public void exitAccount() {
+		
+	}
+	public void sendMessage(Message message) {
+		
+	}
+	public void withdraw() {
+		
+	}
+	public void deposit() {
+		
+	}
+	public void viewBalance() {
+		
+	}
+	public void viewTransactionHist() {
+		
+	}
+	public void createNewCustomer() {
+		
+	}
+	public void createNewBankAccount() {
+		
+	}
+	public void closeBankAccount() {
+		
+	}
+	public void addUserToBankAccount() {
+		
+	}
+	public void removeUserFromBankAccount() {
+		
+	}
+	public void updateCustomerInfo() {
+		
+	}
+	public void callManager() {
+
+	}
+}
