054f184bb6256a1a3f377378ecd781382539e7a1
obwajui <74273976+obwajui@users.noreply.github.com>
2025-04-22 13:30:22 -0700

    Update Server.java
    add a helper function to send messages cleaner

--- implentation/Server.java
+++ implentation/Server.java
@@ -79,61 +79,80 @@ public class Server {
 			//variables to hold data to change
 			Message message;
 			Boolean LOGGEDIN = false;
-			String User = "";
-	        
-			//while the connection is still receiving messages
-	        while((message = (Message) objectInputStream.readObject()) != null) {
-	        	//print the action requested by the client
-	        	System.out.println(
-	        			//display ip of client
-	        			"Client <" + clientSocket.getInetAddress() + "> "
-	        			//if the client is logged in include their name
-	        			+ (LOGGEDIN? "[" + User + "]: " : ": ")
-	        			//display the message type
-	        			+ "Request " + message.getType().name() + " "
-	        			//display any extra info sent with the message
-	        			+ "with data \"" + message.getData() + "\"");
-	        	
-	        	//if the user isn't logged in yet but is making a request to
-	        	if(!LOGGEDIN && (message.getType() == Message.Type.LOGINREQATM || message.getType() == Message.Type.LOGINREQTELLER)) {
-	        		//log them in
-	        		LOGGEDIN = true;
-	        		User = message.getData();
-	        		//respond that the login was successful
-	        		objectOutputStream.writeObject(
-	        				new Message(Message.Type.RESPONSE,  Message.Status.SUCCESS, "Login successful"));
-	        		//go back to waiting for new message
-	        		continue;
-	        	}	
-		        
-	        	//if a message is sent but they are not logged in just throw back an error
-	        	if(!LOGGEDIN) {
-	        		//tell them its not valid
-	        		objectOutputStream.writeObject(
-	        				new Message(Message.Type.RESPONSE,  Message.Status.FAILURE, "A Login request needs to be sent first"));
-	        		//go back to waiting for new message
-	        		continue;
-		        }
-
-	        	
-	        	//below this is where commands for logged in users go
-	        	
-	        	
-	        	
-	        	
-	        	
-	        	
-	        	
-	        	
-	        	//this will be below all other request types and should only be reachable
-	        	//if a message with an incorrect or invalid type is sent
-        		objectOutputStream.writeObject(
-        				new Message(Message.Type.RESPONSE,  Message.Status.FAILURE, "The sent command could not be handeled"));
-	        	
-	        	objectOutputStream.flush();
-	        }//while
+			String User = "";			//TODO: change this to be based on customer data
+	        try {
+				//while the connection is still receiving messages
+		        while((message = (Message) objectInputStream.readObject()) != null) {
+		        	//print the action requested by the client
+		        	System.out.println(
+		        			//display ip of client
+		        			"Client <" + clientSocket.getInetAddress() + "> "
+		        			//if the client is logged in include their name
+		        			+ (LOGGEDIN? "[" + User + "]: " : ": ")
+		        			//display the message type
+		        			+ "Request " + message.getType().name() + " "
+		        			//display any extra info sent with the message
+		        			+ "with data \"" + message.getData() + "\"");
+		        	
+		        	//if the user isn't logged in yet but is making a request to
+		        	if(!LOGGEDIN && (message.getType() == Message.Type.LOGINREQATM || message.getType() == Message.Type.LOGINREQTELLER)) {
+		        		//log them in
+		        		LOGGEDIN = true;
+		        		User = message.getData();
+		        		//respond that the login was successful
+		        		sendMessage(
+		        				new Message(
+		        						"Server", clientSocket.getInetAddress().toString(), "Login successful", Message.Type.LOGINOK));
+		        		//go back to waiting for new message
+		        		continue;
+		        	}	
+			        
+		        	//if a message is sent but they are not logged in just throw back an error
+		        	if(!LOGGEDIN) {
+		        		//tell them its not valid
+		        		sendMessage(
+		        				new Message(
+		        						"Server", clientSocket.getInetAddress().toString(), "Login First", Message.Type.INVALID));
+		        		//go back to waiting for new message
+		        		continue;
+			        }
+	
+		        	
+		        	//below this is where commands for logged in users go
+		        	
+		        	
+		        	
+		        	
+		        	
+		        	
+		        	
+		        	
+		        	
+		        	
+		        	
+		        	
+		        	//this will be below all other request types and should only be reachable
+		        	//if a message with an incorrect or invalid type is sent
+		        	sendMessage(
+	        				new Message(
+	        						"Server", clientSocket.getInetAddress().toString(), "Login First", Message.Type.INVALID));
+		        	
+		        	objectOutputStream.flush();
+		        }//while
+	        }//try
+	        catch(IOException e) {
+	        	e.printStackTrace();
+	        } catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
 	        
 			
+		}
+		
+		//helper function for sending messages
+		private void sendMessage(Message message) throws IOException {
+			objectOutputStream.writeObject(message);
+			objectOutputStream.flush();
 		}
 		
 	}
