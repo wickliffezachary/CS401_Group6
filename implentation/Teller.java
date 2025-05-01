972ccc8273a875ff4f51275e5b27cb69ddf8a170
SpeedRelativity <necharkc5150@gmail.com>
2025-04-30 22:00:09 -0700

    working on gui, bugged but wrote some code + fixed server code

--- implentation/Teller.java
+++ implentation/Teller.java
@@ -1,63 +1,82 @@
-import java.io.*;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.net.InetAddress;
-
 
 public class Teller {
+	
+	private TellerListener listener;
+	private Socket socket = null;
+   	private ObjectInputStream objectInputStream = null;
+   	private ObjectOutputStream objectOutputStream = null;
+	private static int ct=0;
+	private String me;
+	private boolean loggedinteller;
 
-	public static void main(String[] args) {
-		Teller teller = new Teller(InetAddress.getLocalHost(), 1234);
-		TellerGUI = new TellerGUI(teller);
-
-	}
-
-
-	public Teller(String serverAddress, int port) {
-		private Socket socket;
-		private ObjectOutputStream out;
-		Private ObjectInputStream in;
-		private String tellerId = "";
-		private boolean loggedIn = false;
-
-		// setup a client-server connection.
-		try {
-			socket = new Socket(serverAddress, port);
-            out = new ObjectOutputStream(socket.getOutputStream());
-            in = new ObjectInputStream(socket.getInputStream());
-
-			// create a message for teller login and send it.
-			Message loginRequest = new Message("Teller", "Server", "Teller requesting to login", "LOGINREQTELLER")
-			sendMessage(loginReqest);
-
-			Message response = (Message) in.readObject();
-
-			if (response.getType() == Message.Type.LOGINOK) {
-                loggedIn = true;
-                System.out.println("Teller login successful.");
-            } else {
-                System.out.println("Teller login failed.");
-                socket.close();
-            }
-		}
-
-		catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-		
+	public interface TellerListener {
+        	void receivedMessage(Message msg);
 	}
 	
-	public void parseRecMessage() {
-		
+	public Teller(String host, int port, TellerListener listener) throws IOException {
+		this.listener=listener;
+		this.socket = new Socket(host, port);
+		this.objectInputStream = new ObjectInputStream(socket.getInputStream());
+	    this.objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+	        
+	        ct+=1;
+		this.me="Teller"+ct;
+		this.loggedinteller=false;
 	}
-	public void login() {
-		
+	
+	//helper to read in a message 
+	private Message parseRecMessage() throws IOException {
+		Message temp = null;
+		try {
+			//read in message
+			temp = (Message) objectInputStream.readObject();
+		} 
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return temp;
 	}
-	public void logout() {
-		
+
+	private void sendMessage(Message message) throws IOException {
+		//write object to server
+		objectOutputStream.writeObject(message);
+		//flush to keep stream clear
+		objectOutputStream.flush();
 	}
+	
+	public void login(String tellerid, String pswd) throws IOException {
+		//received custname, phno, pswd from gui 
+		String login_creds="uname="+tellerid+",pswd="+pswd;
+		//send message to server
+		sendMessage(new Message(me, "Server", login_creds, Message.Type.LOGINREQTELLER));
+		//wait for server response message
+		//if loginok type message, 
+		Message serverresp = parseRecMessage();
+		if (serverresp.getType()==Message.Type.LOGINOK){
+			loggedinteller=true;
+			//and trigger gui by also sending contents of data field (list of bank accounts of customer) of message 
+		}
+		else if (serverresp.getType()==Message.Type.LOGINDENIED){
+			//elif logindenied type message, trigger gui to displaycreds  error
+		}
+		else{
+			//trigger gui error
+		}
+	}
+	
+	public void logout()  throws IOException {
+		sendMessage(new Message(me, "Server", "Requesting logout", Message.Type.LOGOUTREQTELLER));
+		//wait for server ok or not??????
+		//if logoutok type message, 
+		loggedinuteller=false;
+		//send gui to login page
+		//else err
+	}
+	
 	public void selectCustomer() {
 		
 	}
@@ -70,10 +89,7 @@ public class Teller {
 	public void exitAccount() {
 		
 	}
-	public void sendMessage(Message message) {
-		out.writeObject(message);
-		out.flush();
-	}
+	
 	public void withdraw() {
 		
 	}
@@ -105,6 +121,49 @@ public class Teller {
 		
 	}
 	public void callManager() {
+		//lolz does nothing
+		//ok, maybe a gui lie saying "manager alerted, please wait for assistance"
+	}
 
+	//for testing server-client prior to having our GUI
+	public void testlogin(String tellerid, String pswd) throws IOException {
+		//received custname, phno, pswd from gui 
+		String login_creds="uname="+tellerid+",pswd="+pswd;
+		//send message to server
+		sendMessage(new Message(me, "Server", login_creds, Message.Type.LOGINREQTELLER));
+		//wait for server response message
+		Message serverresp = parseRecMessage();
+		if (serverresp.getType()==Message.Type.LOGINOK){
+		//if loginok type message, 
+		loggedinteller=true;
+		
+		//and trigger gui by also sending contents of data field (list of bank accounts of customer) of message 
+		System.out.println("loggedin");
+		}
+		else if (serverresp.getType()==Message.Type.LOGINDENIED){
+		
+
+		System.out.println("Incorrect creds");
+		}
+		else{
+		System.out.println("some error, check more");
+		}
+		//and spawn gui thread for autologout
+		//elif logindenied type message, trigger gui to display error
+	}
+	public void testlogout() throws IOException {
+		sendMessage(new Message(me, "Server", "Requesting logout", Message.Type.LOGOUTREQTELLER));
+		//wait for server ok or not?
+		Message serverresp = parseRecMessage();
+		if (serverresp.getType()==Message.Type.LOGOUTOK){
+		//if logoutok type message, 
+		loggedinteller=false;
+		//and trigger gui by also sending contents of data field (list of bank accounts of customer) of message 
+		System.out.println("loggedout");
+		}
+		else {
+		System.out.println("some error, check more");
+		}
+		//send gui to login page
 	}
 }
---
+++ implentation/TellerApp.java
@@ -0,0 +1,18 @@
+
+public class TellerApp {
+
+	public static void main(String[] args) {
+		try {
+			System.out.println("Hii");
+			Teller teller = new Teller("127.0.0.1", 1234, msg -> {});
+			
+			 teller.testlogin("Nkc2004", "cs411");
+		}
+		
+		catch (Exception e) {
+	        e.printStackTrace();
+	    }
+
+	}
+
+}
