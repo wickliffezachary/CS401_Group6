f205cd84f9367962cdc66e42ce783f130c086439
harvendhanota <harvendhanota@hotmail.com>
2025-05-01 14:44:10 -0700

    Fixed formatting and fixed incorrect variable name

--- implentation/Teller.java
+++ implentation/Teller.java
@@ -9,23 +9,22 @@ public class Teller {
 	private Socket socket = null;
    	private ObjectInputStream objectInputStream = null;
    	private ObjectOutputStream objectOutputStream = null;
-	private static int ct=0;
+	private static int count = 0;
 	private String me;
-	private boolean loggedinteller;
+	private boolean loggedInTeller;
 
 	public interface TellerListener {
-        	void receivedMessage(Message msg);
+        void receivedMessage(Message msg);
 	}
 	
 	public Teller(String host, int port, TellerListener listener) throws IOException {
-		this.listener=listener;
+		this.listener = listener;
 		this.socket = new Socket(host, port);
-
 		this.objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
 		this.objectInputStream = new ObjectInputStream(socket.getInputStream());
-	        ct+=1;
-		this.me="Teller"+ct;
-		this.loggedinteller=false;
+		count += 1;
+		this.me = "Teller" + count;
+		this.loggedInTeller = false;
 	}
 	
 	//helper to read in a message 
@@ -35,8 +34,8 @@ public class Teller {
 			//read in message
 			temp = (Message) objectInputStream.readObject();
 		} 
-		catch (ClassNotFoundException e) {
-			e.printStackTrace();
+		catch (ClassNotFoundException error) {
+			error.printStackTrace();
 		}
 		return temp;
 	}
@@ -48,19 +47,19 @@ public class Teller {
 		objectOutputStream.flush();
 	}
 	
-	public void login(String tellerid, String pswd) throws IOException {
+	public void login(String tellerID, String password) throws IOException {
 		//received custname, phno, pswd from gui 
-		String login_creds="uname="+tellerid+",pswd="+pswd;
+		String loginCreds = "username=" + tellerID + ",password=" + password;
 		//send message to server
-		sendMessage(new Message(me, "Server", login_creds, Message.Type.LOGINREQTELLER));
+		sendMessage(new Message(me, "Server", loginCreds, Message.Type.LOGINREQTELLER));
 		//wait for server response message
 		//if loginok type message, 
-		Message serverresp = parseRecMessage();
-		if (serverresp.getType()==Message.Type.LOGINOK){
-			loggedinteller=true;
+		Message serverResp = parseRecMessage();
+		if (serverResp.getType() == Message.Type.LOGINOK){
+			loggedInTeller=true;
 			//and trigger gui by also sending contents of data field (list of bank accounts of customer) of message 
 		}
-		else if (serverresp.getType()==Message.Type.LOGINDENIED){
+		else if (serverResp.getType() == Message.Type.LOGINDENIED){
 			//elif logindenied type message, trigger gui to displaycreds  error
 		}
 		else{
@@ -72,7 +71,7 @@ public class Teller {
 		sendMessage(new Message(me, "Server", "Requesting logout", Message.Type.LOGOUTREQTELLER));
 		//wait for server ok or not??????
 		//if logoutok type message, 
-		loggedinuteller=false;
+		loggedInTeller = false;
 		//send gui to login page
 		//else err
 	}
@@ -80,12 +79,15 @@ public class Teller {
 	public void selectCustomer() {
 		
 	}
+	
 	public void exitCustomer() {
 		
 	}
+	
 	public void selectAccount() {
 		
 	}
+	
 	public void exitAccount() {
 		
 	}
@@ -93,75 +95,85 @@ public class Teller {
 	public void withdraw() {
 		
 	}
+	
 	public void deposit() {
 		
 	}
+	
 	public void viewBalance() {
 		
 	}
+	
 	public void viewTransactionHist() {
 		
 	}
+	
 	public void createNewCustomer() {
 		
 	}
+	
 	public void createNewBankAccount() {
 		
 	}
+	
 	public void closeBankAccount() {
 		
 	}
+	
 	public void addUserToBankAccount() {
 		
 	}
+	
 	public void removeUserFromBankAccount() {
 		
 	}
+	
 	public void updateCustomerInfo() {
 		
 	}
+	
 	public void callManager() {
 		//lolz does nothing
 		//ok, maybe a gui lie saying "manager alerted, please wait for assistance"
 	}
 
-
 	//for testing server-client prior to having our GUI
-	public void testlogin(String tellerid, String pswd) throws IOException {
-		//received custname, phno, pswd from gui 
-		String login_creds="uname="+tellerid+",pswd="+pswd;
+	public void testLogin(String tellerID, String password) throws IOException {
+		//received customer name, phone number, password from gui 
+		String loginCreds = "username=" + tellerID + ",password=" + password;
 		//send message to server
-		sendMessage(new Message(me, "Server", login_creds, Message.Type.LOGINREQTELLER));
+		sendMessage(new Message(me, "Server", loginCreds, Message.Type.LOGINREQTELLER));
 		//wait for server response message
-		Message serverresp = parseRecMessage();
-		if (serverresp.getType()==Message.Type.LOGINOK){
-		//if loginok type message, 
-		loggedinteller=true;
+		Message serverResp = parseRecMessage();
+		if (serverResp.getType() == Message.Type.LOGINOK){
+			//if loginok type message, 
+			loggedInTeller = true;
 		
-		//and trigger gui by also sending contents of data field (list of bank accounts of customer) of message 
-		System.out.println("loggedin");
+			//and trigger gui by also sending contents of data field (list of bank accounts of customer) of message 
+			System.out.println("loggedin");
 		}
-		else if (serverresp.getType()==Message.Type.LOGINDENIED){
-		System.out.println("Incorrect creds");
+		else if (serverResp.getType() == Message.Type.LOGINDENIED){
+			System.out.println("Incorrect credentials");
 		}
 		else{
-		System.out.println("some error, check more");
+			System.out.println("some error, check more");
 		}
 		//and spawn gui thread for autologout
 		//elif logindenied type message, trigger gui to display error
 	}
-	public void testlogout() throws IOException {
+	
+	public void testLogout() throws IOException {
 		sendMessage(new Message(me, "Server", "Requesting logout", Message.Type.LOGOUTREQTELLER));
 		//wait for server ok or not?
-		Message serverresp = parseRecMessage();
-		if (serverresp.getType()==Message.Type.LOGOUTOK){
-		//if logoutok type message, 
-		loggedinteller=false;
-		//and trigger gui by also sending contents of data field (list of bank accounts of customer) of message 
-		System.out.println("loggedout");
+		Message serverResp = parseRecMessage();
+		if (serverResp.getType()==Message.Type.LOGOUTOK){
+			//if logoutok type message, 
+			loggedInTeller=false;
+			//and trigger gui by also sending contents of data field (list of bank accounts of customer) of message 
+			System.out.println("loggedout");
 		}
 		else {
-		System.out.println("some error, check more");
+			System.out.println("some error, check more");
 		}
 		//send gui to login page
 	}
