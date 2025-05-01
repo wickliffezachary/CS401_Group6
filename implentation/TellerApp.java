84e971853dd8ba79137ff71c16ecc4e9203cd733
harvendhanota <harvendhanota@hotmail.com>
2025-05-01 14:45:51 -0700

    Corrected method names

--- implentation/TellerApp.java
+++ implentation/TellerApp.java
@@ -1,20 +1,20 @@
-
 public class TellerApp {
 
 	public static void main(String[] args) {
+
 		try {
 			System.out.println("Hii");
-			
+
 			Teller teller = new Teller("127.0.0.1", 1234, msg -> {});
-			
-			 teller.testlogin("Nkc2004", "cs411");
-			 teller.testlogout();
-			 Thread.sleep(500);
+
+			teller.testLogin("Nkc2004", "cs411");
+			teller.testLogout();
+			Thread.sleep(500);
 		}
-		
-		catch (Exception e) {
-	        e.printStackTrace();
-	    }
+
+		catch (Exception error) {
+			error.printStackTrace();
+		}
 
 	}
 
