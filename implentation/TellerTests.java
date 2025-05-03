f5d71f80ecea9fb9ebc2aa9e578a0c371a5e5836
harvendhanota <harvendhanota@hotmail.com>
2025-05-03 00:22:08 -0700

    Implemented constructor test

--- implentation/TellerTests.java
+++ implentation/TellerTests.java
@@ -1,11 +1,33 @@
+import java.io.IOException;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class TellerTests {
     
+	// Fields:
+	private final String host = "localhost";
+	private final int port = 1234;
+	TestListener listener = new TestListener();
+	
+	private class TestListener implements Teller.TellerListener {
+		@Override
+		public void receivedMessage(Message msg) {
+			// TODO - auto-generated method stub
+		}
+	}
+	
 	@Test
-	public void dummyTest() {
-		Assertions.fail();
+	public void ConstructorTest() {
+		Teller teller = null;
+		
+		try {
+			teller = new Teller(host, port, listener);
+		} 
+		catch (IOException error) {
+			error.printStackTrace();
+		}
+		
+		Assertions.assertNotNull(teller);
 	}
 	
 }
