0bb37292d2b57cdd54716daa552b1a12a0ceb5d2
harvendhanota <harvendhanota@hotmail.com>
2025-05-03 00:52:28 -0700

    Implemented login test

--- implentation/TellerTests.java
+++ implentation/TellerTests.java
@@ -1,13 +1,16 @@
 import java.io.IOException;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.Assume;
 
 public class TellerTests {
     
 	// Fields:
-	private final String host = "localhost";
-	private final int port = 1234;
+	private String host = "localhost";
+	private int port = 1234;
 	TestListener listener = new TestListener();
+	private String username = "Nkc2004";
+	private String password = "cs411";
 	
 	private class TestListener implements Teller.TellerListener {
 		@Override
@@ -28,6 +31,116 @@ public class TellerTests {
 		}
 		
 		Assertions.assertNotNull(teller);
+	}
+	
+	@Test
+	public void loginTest() {
+		Teller teller = null;
+		
+		try {
+			teller = new Teller(host, port, listener);
+			teller.testLogin(username, password);
+			
+			Assertions.assertTrue(teller.isLoggedInTeller());
+		}
+		catch (IOException error) {
+			Assume.assumeNoException(error);
+		}
+	}
+	
+	@Test
+	public void logoutTest() {
+		Assertions.fail();
+	}
+	
+	@Test
+	public void selectCustomerTest() {
+		// TODO - method not yet implemented in Teller.java
+		Assertions.fail();
+	}
+	
+	@Test
+	public void exitCustomerTest() {
+		// TODO - method not yet implemented in Teller.java
+		Assertions.fail();
+	}
+	
+	@Test
+	public void selectAccountTest() {
+		// TODO - method not yet implemented in Teller.java
+		Assertions.fail();
+	}
+	
+	@Test
+	public void exitAccountTest() {
+		// TODO - method not yet implemented in Teller.java
+		Assertions.fail();
+	}
+	
+	@Test
+	public void withdrawTest() {
+		// TODO - method not yet implemented in Teller.java
+		Assertions.fail();
+	}
+	
+	@Test
+	public void depositTest() {
+		// TODO - method not yet implemented in Teller.java
+		Assertions.fail();
+	}
+	
+	@Test
+	public void viewBalanceTest() {
+		// TODO - method not yet implemented in Teller.java
+		Assertions.fail();
+	}
+	
+	@Test
+	public void viewTransactionHistoryTest() {
+		// TODO - method not yet implemented in Teller.java
+		Assertions.fail();
+	}
+	
+	@Test
+	public void createNewCustomerTest() {
+		// TODO - method not yet implemented in Teller.java
+		Assertions.fail();
+	}
+	
+	@Test
+	public void createNewBankAccountTest() {
+		// TODO - method not yet implemented in Teller.java
+		Assertions.fail();
+	}
+	
+	@Test
+	public void closeBankAccountTest() {
+		// TODO - method not yet implemented in Teller.java
+		Assertions.fail();
+	}
+	
+	@Test
+	public void addUserToBankAccountTest() {
+		// TODO - method not yet implemented in Teller.java
+		Assertions.fail();
+	}
+	
+	@Test
+	public void removeUserFromBankAccountTest() {
+		// TODO - method not yet implemented in Teller.java
+		Assertions.fail();
+	}
+	
+	@Test
+	public void updateCustomerInfoTest() {
+		// TODO - method not yet implemented in Teller.java
+		Assertions.fail();
+	}
+	
+	@Test
+	public void callManagerTest() {
+		// TODO - method not yet implemented in Teller.java
+		Assertions.fail();
 	}
 	
 }
