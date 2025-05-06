7f7170bf904cb6259a6346ac53fb58473c7a958d
harvendhanota <harvendhanota@hotmail.com>
2025-05-05 18:37:04 -0700

    Added implementation for deposit, withdraw, and save tests

--- implentation/BankAccountTests.java
+++ implentation/BankAccountTests.java
@@ -26,34 +26,60 @@ public class BankAccountTests {
 	}
 	
 	@Test
-	public void checkInActiveStatusTest() {
+	public void checkInActiveAccessTest() {
 		BankAccount testAccount = new BankAccount(username, checking);
 		
 		Assertions.assertFalse(testAccount.checkInActiveAccess());
 	}
 	
 	@Test
-	public void addUserTest() {
-		// TODO - implement this test
-		Assertions.fail();
-	}
-	
-	@Test
-	public void removeUserTest() {
-		// TODO - implement this test
-		Assertions.fail();
-	}
-	
-	@Test
-	public void renameUserTest() {
-		// TODO - implement this test
-		Assertions.fail();
-	}
-	
-	@Test
-	public void getAccountIDTest() {
-		BankAccount testAccount = new BankAccount(username, checking);
+	public void getBalanceTest() {
+		BankAccount testAccount = new BankAccount(username, savings);
+		testAccount.updateBalance(420.69);
 		
-		Assertions.assertEquals("40", testAccount.getAccountID());
+		Assertions.assertEquals(420.69, testAccount.getBalance());
+	}
+	
+	@Test
+	public void getTypeTest() {
+		BankAccount testAccount = new BankAccount(username, savings);
+		
+		Assertions.assertEquals(savings, testAccount.getType());
+	}
+	
+	@Test
+	public void switchAccessTest() {
+		BankAccount testAccount = new BankAccount(username, checking);
+		testAccount.switchAccess();
+		
+		Assertions.assertTrue(testAccount.checkInActiveAccess());
+	}
+	
+	@Test
+	public void depositTest() {
+		BankAccount testAccount = new BankAccount(username, checking);
+		testAccount.deposit(420.69);
+		testAccount.deposit(69.42);
+		
+		Assertions.assertEquals(420.69 + 69.42, testAccount.getBalance());
+	}
+	
+	@Test
+	public void withdrawTest() {
+		BankAccount testAccount = new BankAccount(username, savings);
+		testAccount.updateBalance(1000.00);
+		testAccount.withdraw(420.69);
+		
+		Assertions.assertEquals(1000.00 - 420.69, testAccount.getBalance());
+	}
+	
+	@Test
+	public void saveTest() {
+		BankAccount testAccount = new BankAccount(username, checking);
+		testAccount.save();
+		
+		File testFile = new File(System.getProperty("user.dir") + "/data/bankAccounts/" + testAccount.getAccountID() + ".txt");
+		
+		Assertions.assertTrue(testFile.exists());
 	}
 }
